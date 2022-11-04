01c773b1a69e789a571eef45cde0e0caf26f7dfc
Bianca0909 <biancamedeiros0909@gmail.com>
2022-11-04 20:11:46 -0300

    new commit

--- src/main/java/dao/PessoaDao.java
+++ src/main/java/dao/PessoaDao.java
@@ -1,5 +1,26 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import config.ConfigConexao;
+import domain.Pessoa;
+
 public class PessoaDao {
 
+	private String sqlInsert = "INSERT INTO PESSOA(nome, apelido) values(?, ?)";
+
+	public void inserir(Pessoa p) {
+		try {
+			Connection connection = ConfigConexao.getConexao(); // Chama conexão com o database
+			PreparedStatement preparedStatement = connection.prepareStatement(sqlInsert); // chama o comando
+			
+			preparedStatement.setString(1, p.getNome()); // Seta os dados
+			preparedStatement.setString(2, p.getApelido());
+			preparedStatement.execute(); // Executa o sql
+			
+		} catch (Exception e) {
+			System.err.print(e.getMessage()); // Mensagem de erro caso o Try der errado
+		}
+	}
 }
